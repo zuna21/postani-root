8dbeb52f3b8a3a3360ebba0173e0d9bac97f9be0
rabbit <rabbit@kali.168.1.5>
2022-08-05 18:09:20 +0200

    login and register style

--- postaniRoot/settings.py
+++ postaniRoot/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '192.168.1.5']
 
 
 # Application definition
